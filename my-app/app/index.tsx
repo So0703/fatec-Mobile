773cd1b7d385efa3bee68cd70d2550df9cded4e5
Sabrina Onishi <90432003+So0703@users.noreply.github.com>
2024-08-22 00:21:20 +0000

    Novo button com gluestack

--- my-app/app/index.tsx
+++ my-app/app/index.tsx
@@ -1,10 +1,10 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, Image } from 'react-native';
 import StyledInput from '../components/styled-input';
-import StyledButton from '../components/styled-input/button';
 import { router } from 'expo-router'
 import React from 'react';
 import DisplayAnImage from '../components/styled-input/image';
+import { Button, ButtonText } from '@/components/ui/button'
 
 export default function Login() {
 
@@ -20,8 +20,9 @@ const handleLogin = () => {
       onChangeText={(text) => console.log(text)} />
       <StyledInput placeholder="Senha"
       onChangeText={(text) => console.log(text)} />
-      <StyledButton text="Acessar o App"
-      onClick={handleLogin} />
+      <Button size='md' variant='solid' onPress={handleLogin}>
+        <ButtonText> Acessar o App </ButtonText>
+        </Button>
       <StatusBar style="auto" />
     </View>
   );
